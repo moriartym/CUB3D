c84ba78655d30aad50c19103404143955d656e10
darrenfasius <darrenfasiuslim@gmail.com>
2025-01-19 14:32:36 +0800

    cub3d

---
+++ cub3d.h
@@ -0,0 +1,8 @@
+#include <fcntl.h>       // open, close
+#include <unistd.h>      // read, write, close
+#include <stdio.h>       // printf, perror
+#include <stdlib.h>      // malloc, free, exit
+#include <string.h>      // strerror
+#include <sys/time.h>    // gettimeofday
+#include <math.h>        //gcc your_program.c -lm
+#include <stdio.h>
